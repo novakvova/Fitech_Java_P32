5b0ea41548e5f5dc5819a732c134bce6bab6c094
novakvova <novakvova@gmail.com>
2026-04-30 17:50:00 +0300

    Working is good app

--- Lesson 9.Sprint Boot/1.SimpleProject/src/main/java/org/example/controllers/HomeController.java
+++ Lesson 9.Sprint Boot/1.SimpleProject/src/main/java/org/example/controllers/HomeController.java
@@ -1,12 +1,20 @@
 package org.example.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.example.repositories.IGenreRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final IGenreRepository genreRepository;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        //Список усіх жанрів
+        var list = genreRepository.findAll();
+        model.addAttribute("genres", list);
         return "index";
     }
 }
--- Lesson 9.Sprint Boot/1.SimpleProject/src/main/java/org/example/data/AppSeedData.java
+++ Lesson 9.Sprint Boot/1.SimpleProject/src/main/java/org/example/data/AppSeedData.java
@@ -2,17 +2,40 @@ package org.example.data;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import net.datafaker.Faker;
+import org.example.entities.Genre;
+import org.example.repositories.IGenreRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
 public class AppSeedData {
-
-
-    //Цей метод буде Seed даних у БД
-    //Цей метод в java Spring буде зпускати автоматично
+    //final - теж саме, що readonly у С#
+    private final IGenreRepository genreRepository;
+    private final Faker faker = new Faker(new Locale("uk"));
     @PostConstruct
     public void seed() {
         System.out.println("---------Run seed data-----------");
+        if(genreRepository.count() == 0)
+        {
+            List<String> genres = new ArrayList<>();
+            int n = 10, i=0;
+            while(i<n) {
+                String genreName = faker.music().genre();
+                if(!genres.contains(genreName)) {
+                    genres.add(genreName);
+                    i++;
+                }
+            }
+            for (String genreName : genres) {
+                Genre genre = new Genre();
+                genre.setName(genreName);
+                genreRepository.save(genre);
+            }
+        }
     }
 }
