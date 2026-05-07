1a42ca5b9b77faf7ef43ebd91f70313e3835cc60
novakvova <novakvova@gmail.com>
2026-05-07 16:41:10 +0300

    Working security

---
+++ Lesson 9.Sprint Boot/1.SimpleProject/src/main/java/org/example/entities/UserEntity.java
@@ -0,0 +1,59 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "users")
+@Data
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    @Email(message = "Invalid email")
+    private String email;
+
+    @Column(nullable = true, length = 200)
+    private String image;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = true)
+    private String resetPasswordToken;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<RoleEntity> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(r -> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+}
